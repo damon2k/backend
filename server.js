ea10fcef702d96fc81c19dafb36820d59890b63f
Harshvardhan Pratap Singh <harshvardhan3@icloud.com>
2025-09-08 17:57:45 +0530

    adding vercel json

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000", // React app URL
+    origin: "https://client-six-gamma-54.vercel.app/", // React app URL
     methods: ["GET", "POST"]
   }
 });
